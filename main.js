aa94eac996b2d9c0101203fd2f5f37d686bf9d1d
Bhanu Teja <sunny.panguluri@gmail.com>
2020-12-03 17:23:27 +0530

    Final

---
+++ main.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json";
+
+function getTranslationURL(text)
+{
+    return serverURL+ "?" + "text=" + text;
+}
+function clickEventHandler()
+{
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(
+        json => {
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+        });
+}
+
+btnTranslate.addEventListener('click',clickEventHandler);
